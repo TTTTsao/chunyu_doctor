4bb757461b56916ee78b856ff44aa778bac0eaa6
Zoe Cho <zoetsao010320@163.com>
2023-02-19 21:44:15 +0800

    fix: crawl_task

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 from spider.task.crawl_task import *
+from spider.util.log_util import create_crawl_logger
+logger = create_crawl_logger()
 
 if __name__ == '__main__':
-    # crawl_doctor_high_fruency_info_task()
-    crawl_realtime_inquiry_task()
-    # crawl_doctor_status_task()
+    crawl_doctor_question_task()
